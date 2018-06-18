4a17bba3755d61a58f26c9e7809e3d0e1c2f5de8
Dev Intern 01 <devintern01@Dev-Intern-01.local>
2018-06-18 09:56:01 -0400

    updates

--- nyt.py
+++ nyt.py
@@ -1,6 +1,13 @@
 from nytimesarticle import articleAPI
 api = articleAPI("391f118fad27467d8b193a6686e1b3d1")
 
-articles = api.search(q = 'Obama',
-    fq = {'headline':'Obama', 'source': ['Reuters','The New York Times']},
-    begin_date = 20111231, page = 3)
+articles = api.search(q = 'Trump',
+    fq = {'headline':'Trump', 'source': ['The New York Times']},
+    begin_date = 20180618)
+
+
+
+print articles
+
+
+#prints the url to the trending article and the details of the whole article
--- tweet.py
+++ tweet.py
@@ -1,16 +1,17 @@
 from TwitterAPI import TwitterAPI
 
-
 consumer_key = 'znFlqUAAnZOsls1QmaQamm0Q0'
 consumer_secret = '7FV3OhO6JZhM6sbaKPq5ckNSf8Zq3vqRiMXy4kEDylB7Be45th'
 token = "704777239204749312-Ikxdp9qPAErYz7tmAYlQ5x4mYZxtYzf"
 token_secret = "6IEyoRzH5taO4Q2pZMy7RZUqZNhvti97WogJlHxbv3JIt"
-track_term = "Radical Media"
 
 api = TwitterAPI(consumer_key, consumer_secret, token, token_secret)
 
-r= api.request('statuses/filter', {'locations':'-74,40,-73,41', 'track': track_term, 'count':100})
-for item in r:
-    print(item['text'] if 'text' in item else item)
+SEARCH_TERM = "Advertising"
 
-#option to either stream tweets, or post the RadicalMedia timeline
+r = api.request('search/tweets', {'q': SEARCH_TERM})
+
+for item in r:
+    print(item)
+
+print('\nQUOTA: %s' % r.get_quota())
